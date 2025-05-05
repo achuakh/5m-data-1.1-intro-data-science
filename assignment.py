628aa3bad5fc06473d0e6907bd3b9992e6b6487d
achuakh <achuakh@gmail.com>
2025-05-05 20:13:43 +0800

    First commit, one failure

--- assignment.py
+++ assignment.py
@@ -6,16 +6,30 @@
 
 
 def fizz_buzz(number):
-    """Returns Fizz if number is divisible by 3, Buzz if divisible by 5, FizzBuzz if divisible by both 3 and 5.
+
+    """
+    Returns Fizz if number is divisible by 3, Buzz if divisible by 5, FizzBuzz if divisible by both 3 and 5.
     If not divisible by either 3 or 5, returns the number itself.
     >>> fizz_buzz(3)
-    'Fizz'
+    Fizz
     >>> fizz_buzz(5)
-    'Buzz'
+    Buzz
     >>> fizz_buzz(15)
-    'FizzBuzz'
+    FizzBuzz
     """
-    return
+
+    if not isinstance(number, int):
+        return -1  # Return -1 if the input is not an integer.
+
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        return number  # Returns the number if not divisible by 3 or 5
+    
 
 
 # Question 2
@@ -24,13 +38,23 @@ def fizz_buzz(number):
 
 
 def sum_of_squares(numbers):
-    """Returns the sum of the squares of all the numbers in a list.
+
+    """
+    Returns the sum of the squares of all the numbers in a list.
     >>> sum_of_squares([1, 2, 3])
     14
     >>> sum_of_squares([2, 4, 6])
     56
     """
-    return
+
+    if not isinstance(numbers, list):
+        return -1  # Return -1 if the input is not a list
+
+    sum_of_squares = 0
+    for number in numbers:
+        sum_of_squares += number ** 2  # Square each number and add to the sum
+
+    return sum_of_squares
 
 
 # Question 3
@@ -39,13 +63,26 @@ def sum_of_squares(numbers):
 
 
 def count_vowels(string):
-    """Returns the number of vowels in a string.
+
+    """
+    Returns the number of vowels in a string.
     >>> count_vowels("hello")
     2
     >>> count_vowels("aeiou")
     5
     """
-    return
+
+    if not isinstance(string, str):
+        return -1  # Return -1 if the input is not a string
+
+    vowels = "aeiouAEIOU"  # String containing all vowels (both cases)
+    vowel_count = 0 # reset the count
+
+    for char in string:
+        if char in vowels:
+            vowel_count += 1 # current value + 1
+    return vowel_count
+
 
 
 # Question 4
@@ -54,16 +91,28 @@ def count_vowels(string):
 
 
 def count_repeats(string):
-    """Returns the number of repeated characters in a string.
+
+    """
+    Returns the number of repeated characters in a string.
     >>> count_repeats("hello")
     2
     >>> count_repeats("aeiou")
     0
     """
-    return
+
+    if not isinstance(string, str):
+        return -1
+    counts = {}
+    for char in string:
+        if char in counts:
+            counts[char] += 1
+        else:
+            counts[char] = 1
+
+    repeated_count = sum(count > 1 for count in counts.values()) # Changed this line    
+    return repeated_count
 
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod(verbose=True)
